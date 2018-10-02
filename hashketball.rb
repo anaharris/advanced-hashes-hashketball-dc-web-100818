97c3767e73c64fe876273549ce44f2b6361a7ebd
Ana Harris <vana1990@gmail.com>
2018-10-02 20:10:10 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,9 +135,7 @@ def good_practices
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
       team_data.each do |attribute, data|
         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-    
- 
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+      #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
         data.each do |data_item|
             binding.pry
       end
