2eae9764b2de4dcba0ef4185a28fe2c13d1ea03e
Ana Harris <vana1990@gmail.com>
2018-10-02 20:34:09 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,7 +121,7 @@ end
 def num_points_scored (players_name)
  game_hash.each do |location, team_data|
    binding.pry
-    team_data[:players][players_name][:points]
+  #  team_data[:players][players_name][:points]
 
   end
 end
