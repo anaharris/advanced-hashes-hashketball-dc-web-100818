5ff45e57f8cd54475d428cac563c5447c0f8af8b
Ana Harris <vana1990@gmail.com>
2018-10-02 21:06:00 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,7 +143,7 @@ end
 def team_colors (team_name)
   team = ""
   game_hash.each do |location, team_data|
-    if team_data == team_name
+    if team_data[team_name] = 
       team = team_data
     end
   end
