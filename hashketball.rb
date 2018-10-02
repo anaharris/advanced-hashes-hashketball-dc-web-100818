ce1e913eeeb8cc81b0350696d0b3bf8216f11591
Ana Harris <vana1990@gmail.com>
2018-10-02 20:23:48 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,19 +124,5 @@ def num_points_scored (players_name)
   end
 end
 
-
-
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-      team_data.each do |attribute, data|
-         binding.pry
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-      #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-      end
-    end
-  end
-end
-
-good_practices
+ 
 
