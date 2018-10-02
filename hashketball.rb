207c9af6cbac030ed6b386251493d3048e451a0d
Ana Harris <vana1990@gmail.com>
2018-10-02 21:30:25 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ def player_numbers (team)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team
       team_data[:players].each do |player, info|
-        jersey_numbers << player[:number]
+        jersey_numbers << info[:number]
       end
     end
   end
