f2db8841036ae172b9864a4faf3fa43dfa11a65a
Ana Harris <vana1990@gmail.com>
2018-10-02 21:52:48 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,8 @@ end
 
 def big_shoe_rebounds 
   player_shoe = ""
-  biggest_shoe = 1 
+  biggest_shoe = 1
+  
   game_hash.each do |location, team_data|
     team_data[:players].each do |name, info|
       if info[:shoe] > biggest_shoe
