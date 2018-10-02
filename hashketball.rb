c8ac427ead72d07dcc8a690c5e7a296672de6e1e
Ana Harris <vana1990@gmail.com>
2018-10-02 20:39:48 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,7 +121,7 @@ end
 def num_points_scored (players_name)
   player = ""
  game_hash.each do |location, team_data|
-    if team_data[:players][players_name]
+    if team_data[:players][players_name] == nil 
     
   end
 end
