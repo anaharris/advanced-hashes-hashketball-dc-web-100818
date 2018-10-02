de50efacff8dcfe0043bb713f29c583e9c55a306
Ana Harris <vana1990@gmail.com>
2018-10-02 20:51:31 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,12 +139,3 @@ def shoe_size (players_name)
   return player[:shoe]
 end
 
-def player_numbers (players_name)
-  player = ""
-  game_hash.each do |location, team_data|
-    if team_data[:players][players_name]
-      player = team_data[:players][players_name]
-    end
-  end
-  return player[:number]
-end
